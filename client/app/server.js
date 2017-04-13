709b21a58215163b95ab11406728b227c1888629
Matt Hinsley <mhinsley@umass.edu>
2017-04-13 03:21:44 -0400

    missionAccomplished

--- client/app/server.js
+++ client/app/server.js
@@ -122,28 +122,24 @@ export function postStatusUpdate(user, location, contents, cb) {
 
 
 /**
- * Adds a new comment to the database on the given feed item.
- */
+* Adds a new comment to the database on the given feed item.
+*/
 export function postComment(feedItemId, author, contents, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  feedItem.comments.push({
-    "author": author,
-    "contents": contents,
-    "postDate": new Date().getTime(),
-    "likeCounter": []
+  sendXHR('POST', '/feeditem/' + feedItemId + '/commentthread/comment', {
+    author: author,
+    contents: contents
+  }, (xhr) => {
+    cb(JSON.parse(xhr.responseText));
   });
-  writeDocument('feedItems', feedItem);
-  // Return a resolved version of the feed item.
-  emulateServerReturn(getFeedItemSync(feedItemId), cb);
 }
+
 
 /**
  * Updates a feed item's likeCounter by adding the user to the likeCounter.
  * Provides an updated likeCounter in the response.
  */
 export function likeFeedItem(feedItemId, userId, cb) {
-  sendXHR('PUT', '/feeditem/' + feedItemId + '/likelist/' + userId,
-          undefined, (xhr) => {
+  sendXHR('PUT', '/feeditem/' + feedItemId + '/likelist/' + userId, undefined, (xhr) => {
     cb(JSON.parse(xhr.responseText));
   });
 }
@@ -160,30 +156,23 @@ export function unlikeFeedItem(feedItemId, userId, cb) {
 }
 
 /**
- * Adds a 'like' to a comment.
- */
-export function likeComment(feedItemId, commentIdx, userId, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  var comment = feedItem.comments[commentIdx];
-  comment.likeCounter.push(userId);
-  writeDocument('feedItems', feedItem);
-  comment.author = readDocument('users', comment.author);
-  emulateServerReturn(comment, cb);
+* Adds a 'like' to a comment.
+*/
+export function likeComment(feedItemId, commentId, userId, cb) {
+  sendXHR('PUT', '/feeditem/' + feedItemId + '/commentthread/comment/' + commentId + '/likelist/' + userId,
+  undefined, (xhr) => {
+    cb(JSON.parse(xhr.responseText));
+  });
 }
 
 /**
- * Removes a 'like' from a comment.
- */
-export function unlikeComment(feedItemId, commentIdx, userId, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  var comment = feedItem.comments[commentIdx];
-  var userIndex = comment.likeCounter.indexOf(userId);
-  if (userIndex !== -1) {
-    comment.likeCounter.splice(userIndex, 1);
-    writeDocument('feedItems', feedItem);
-  }
-  comment.author = readDocument('users', comment.author);
-  emulateServerReturn(comment, cb);
+* Removes a 'like' from a comment.
+*/
+export function unlikeComment(feedItemId, commentId, userId, cb) {
+  sendXHR('DELETE', '/feeditem/' + feedItemId + '/commentthread/comment/' + commentId + '/likelist/' + userId,
+  undefined, (xhr) => {
+    cb(JSON.parse(xhr.responseText));
+  });
 }
 
 /**
